b25e6bb08608e0b69bd48ce3f7b8127b1a76c040
James Basile <jbasile145@gmail.com>
2019-04-09 18:18:54 -0400

    forgot to commit changes outside of 'testing-ii' folder

--- testing-ii/src/App.test.js
+++ testing-ii/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App';
+import Display from './components/Display/Display';
+import Dashboard from './components/Dashboard/Dashboard';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+
+  
+})
