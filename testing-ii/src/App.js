b6d1357fbd916b4c458a5d3010fda71515b72c00
James Basile <jbasile145@gmail.com>
2019-04-09 16:29:39 -0400

    init commit, react app created, dependencies added

---
+++ testing-ii/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>James Basile:</h1>
+        <h2>Web Testing II</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
