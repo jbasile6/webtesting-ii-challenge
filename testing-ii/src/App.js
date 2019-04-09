2e7c7417e7c685d0074ac3385e5c968d491569dd
James Basile <jbasile145@gmail.com>
2019-04-09 17:19:57 -0400

    ball, foulball, hit functionality complete

--- testing-ii/src/App.js
+++ testing-ii/src/App.js
@@ -18,6 +18,9 @@ class App extends Component {
       />
       <Dashboard 
       clickStrike={this.throwStrike}
+      clickBall={this.throwBall}
+      clickFoul={this.foulBall}
+      clickHit={this.baseHit}
       />
       </div>
     );
@@ -37,14 +40,51 @@ class App extends Component {
         balls: 0
       })
       console.log('Strike 3! You\'re out!')
+      window.alert('Strike 3! You\'re out!')
     }
   }
 
   //ball is thrown
+  throwBall = () => {
+    if (this.state.balls < 3) {
+      this.setState({
+        strikes: this.state.strikes,
+        balls: this.state.balls += 1
+      })
+      console.log(`Ball ${this.state.balls}!`)
+    } else {
+      this.setState({
+        strikes: 0,
+        balls: 0
+      })
+      console.log('Ball 4! Take your base!')
+      window.alert('Ball 4! Take your base!')
+    }
+  }
 
   //foul ball
+  foulBall = () => {
+    if (this.state.strikes < 2) {
+      this.setState({
+        strikes: this.state.strikes += 1,
+        balls: this.state.balls
+      })
+      console.log(`Foul Ball! The count is now ${this.state.balls}-${this.state.strikes}.`)
+    } else {
+      console.log(`Foul Ball! The count remains ${this.state.balls}-${this.state.strikes}.`)
+      window.alert(`Foul Ball! The count remains ${this.state.balls}-${this.state.strikes}.`)
+    }
+  }
 
   //base hit
+  baseHit = () => {
+    this.setState({
+      strikes: 0,
+      balls: 0
+    })
+    console.log('Base Hit! A new batter is up to the plate.')
+    window.alert('Base Hit! A new batter is up to the plate.')
+  }
 }
 
 export default App;
--- testing-ii/src/components/Dashboard/Dashboard.js
+++ testing-ii/src/components/Dashboard/Dashboard.js
@@ -4,9 +4,9 @@ const Dashboard = (props) => {
     return (
         <div className='dashboard-container'>
         <button className='dash-btn' onClick={props.clickStrike}>Strike</button>
-        <button className='dash-btn'>Ball</button>
-        <button className='dash-btn'>Foul Ball</button>
-        <button className='dash-btn'>Hit</button>
+        <button className='dash-btn' onClick={props.clickBall}>Ball</button>
+        <button className='dash-btn' onClick={props.clickFoul}>Foul Ball</button>
+        <button className='dash-btn' onClick={props.clickHit}>Hit</button>
         </div>
     )
 }
