bc3b5520623175778e03bc8df4b6655d17a1005d
James Basile <jbasile145@gmail.com>
2019-04-09 17:01:49 -0400

    strike functionality added

--- testing-ii/src/App.js
+++ testing-ii/src/App.js
@@ -16,10 +16,35 @@ class App extends Component {
       strikes={this.state.strikes}
       balls={this.state.balls}
       />
-      <Dashboard />
+      <Dashboard 
+      clickStrike={this.throwStrike}
+      />
       </div>
     );
   }
+
+  //strike is thrown
+  throwStrike = () => {
+    if (this.state.strikes < 2) {
+      this.setState({
+        strikes: this.state.strikes += 1,
+        balls: this.state.balls
+      })
+      console.log(`Strike ${this.state.strikes}!`)
+    } else {
+      this.setState({
+        strikes: 0,
+        balls: 0
+      })
+      console.log('Strike 3! You\'re out!')
+    }
+  }
+
+  //ball is thrown
+
+  //foul ball
+
+  //base hit
 }
 
 export default App;
--- testing-ii/src/components/Dashboard/Dashboard.js
+++ testing-ii/src/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = (props) => {
     return (
         <div className='dashboard-container'>
-        <button className='dash-btn'>Strike</button>
+        <button className='dash-btn' onClick={props.clickStrike}>Strike</button>
         <button className='dash-btn'>Ball</button>
         <button className='dash-btn'>Foul Ball</button>
         <button className='dash-btn'>Hit</button>
